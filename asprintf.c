8dbe46a882616e0290126201d2be60211b1672c8
Neved4 <63655535+Neved4@users.noreply.github.com>
2024-11-18 19:46:55 +0100

    Refactor `asprintf.c` formatting

--- asprintf.c
+++ asprintf.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "asprintf.h"
 
 int vasprintf(char **strp, const char *fmt, va_list ap) {
 	int size, res;
-
 	va_list cp;
+
 	va_copy(cp, ap);
 	size = vsnprintf(NULL, 0, fmt, cp);
 	va_end(cp);
-	if (size < 0)
+
+	if (size < 0) {
 		return -1;
+	}
 
 	*strp = malloc(size + 1);
-	if (*strp == NULL)
+	if (*strp == NULL) {
 		return -1;
+	}
 
 	res = vsnprintf(*strp, size + 1, fmt, ap);
 	if (res < 0) {
@@ -33,5 +35,6 @@ int asprintf(char **s, const char *fmt, ...) {
 	va_start(ap, fmt);
 	ret = vasprintf(s, fmt, ap);
 	va_end(ap);
+
 	return ret;
 }
